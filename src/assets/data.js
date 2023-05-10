379e8d754f81478449bba419be6b4840d7629658
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-05-11 01:10:12 +0500

    added Common Symptoms Section

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,12 +2,36 @@ export const categories = [];
 
 // Common Symptoms
 export const symptoms = [
-  'Acidity',
-  'Anxiety',
-  'Back Pain',
-  'Blood Pressure',
-  'Depresssion',
-  'Diabeties',
-  'Fever',
-  'Hair Fall',
+  {
+    text: 'Acidity',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Anxiety',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Back Pain',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Blood Pressure',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Depression',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Diabeties',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'Fever',
+    path: '../../assets/images/blood.png',
+  },
+  {
+    text: 'HairFall',
+    path: '../../assets/images/blood.png',
+  },
 ];
--- src/components/homeComponents/Symptoms.js
+++ src/components/homeComponents/Symptoms.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
-import {symptoms} from '../../assets/data';
 
 export default function Symptoms() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Common Symptoms</Text>
-      <View>
-        {symptoms.map(item => (
-          <Box text={item} src={require('../../assets/images/blood.png')} />
-        ))}
+      <View style={styles.boxes}>
+        <Box
+          text={'Acidity'}
+          src={require('../../assets/images/acidity.png')}
+        />
+        <Box
+          text={'Anxiety'}
+          src={require('../../assets/images/anxiety.png')}
+        />
+        <Box
+          text={'BackPain'}
+          src={require('../../assets/images/backpain.png')}
+        />
+        <Box text={'Blood'} src={require('../../assets/images/blood.png')} />
+        <Box
+          text={'Depression'}
+          src={require('../../assets/images/depression.png')}
+        />
+        <Box
+          text={'Diabeties'}
+          src={require('../../assets/images/diabeties.png')}
+        />
+        <Box text={'Fever'} src={require('../../assets/images/fever.png')} />
+        <Box
+          text={'Hairfall'}
+          src={require('../../assets/images/hairfall.png')}
+        />
       </View>
     </View>
   );
@@ -17,9 +39,11 @@ export default function Symptoms() {
 
 const Box = ({text, src}) => {
   return (
-    <View>
-      <Image source={src} />
-      <Text>{text}</Text>
+    <View style={styles.boxContainer}>
+      <View style={styles.imgContainer}>
+        <Image source={src} style={styles.img} />
+      </View>
+      <Text style={styles.text}>{text}</Text>
     </View>
   );
 };
@@ -32,5 +56,33 @@ const styles = StyleSheet.create({
     color: '#02100F',
     fontSize: 15,
     fontWeight: '500',
+  },
+  boxes: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  boxContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  imgContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginRight: 10,
+    borderRadius: 10,
+    width: 70,
+    height: 70,
+  },
+  img: {
+    width: 45,
+    height: 45,
+    resizeMode: 'cover',
+  },
+  text: {
+    marginVertical: 5,
+    fontSize: 15,
   },
 });
